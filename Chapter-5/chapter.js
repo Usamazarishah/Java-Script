db18cd1a5a1d6f0b15b1ac0a6c66b1df2132f1b2
fg72072 <105040783+fg72072@users.noreply.github.com>
2024-03-02 07:38:01 +0500

    chapter-5 done

---
+++ Chapter-5/chapter.js
@@ -0,0 +1,14 @@
+// Q:1
+
+var num1 = 5
+var num2 = 3
+
+var addition = num1 + num2
+console.log(addition)
+
+document.querySelector(".num1").innerHTML = num1;
+document.querySelector(".num2").innerHTML = num2;
+document.querySelector(".addition").innerHTML = addition;
+
+// Q:2
+var 
