5de1881accc14660bc83c53ac8edc8e99aece3b0
M.Usama <usamazarishah@gmail.com>
2024-03-22 22:31:24 +0500

    For-Loop exercise done

---
+++ For-Loop/app.js
@@ -0,0 +1,21 @@
+// Question 01
+console.log('Write a loop to print numbers from 1 to 10');
+for (var i =1; i <= 10; i++){
+    console.log(i);
+}
+
+//Question 02
+console.log('Write a loop to print even numbers from 1 to 20.');
+var number = 0;
+for (var i =1; i<=10 ; i++){
+    number = number+2
+    console.log(number);
+}
+
+// Question 03
+console.log('Write a loop to print odd numbers from 1 to 15.');
+var number = 1
+for(var i =1; i <=7; i++){
+    number = number+2
+    console.log(number);
+}
