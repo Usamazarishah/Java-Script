f39d2a13570e7ec92140a0c1d4b781dfd0e94814
M.Usama <usamazarishah@gmail.com>
2024-05-17 22:25:26 +0500

    Cow-Gallery

---
+++ Cow-Gallery/app.js
@@ -0,0 +1,25 @@
+
+var preview = document.getElementById('previewImg')
+
+
+var box1 = document.getElementById('box1')
+var box2 = document.getElementById('box2')
+var box3 = document.getElementById('box3')
+var box4 = document.getElementById('box4')
+var box5 = document.getElementById('box5')
+
+box1.addEventListener('click', function () {
+    preview.style.src = this.style.src
+})
+box2.addEventListener('click', function () {
+    preview.style.src = this.style.src
+})
+box3.addEventListener('click', function () {
+    preview.style.src = this.style.src
+})
+box4.addEventListener('click', function () {
+    preview.style.src = this.style.src
+})
+box5.addEventListener('click', function () {
+    preview.style.src = this.style.src
+})
